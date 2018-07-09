59796b5ccb360e2b5b5c23cdec0003a6c5f28193
Bernardo <bernardopereiraf@gmail.com>
2018-07-08 21:25:51 -0300

    Initial commit. Added splash scene from old SceneLoader

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public float autoLoadNextLevelAfter;
+
+    void Start()
+    {
+        Invoke("LoadNextScene", autoLoadNextLevelAfter);
+    }
+
+    public void LoadScene(string name)
+    {
+        Debug.Log("New level Load: " + name);
+        SceneManager.LoadScene(name);
+    }
+
+    public void LoadNextScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex +1);
+    }
+
+    public void LoadStartScene()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void QuitGame()
+    {
+        print("Quit requested");
+        Application.Quit();
+    }
+}
