79781018db0fa9bd1b42360a06cb1120009b9f3e
Bernardo F. Pereira <bernardopereiraf@gmail.com>
2018-07-15 19:59:33 -0300

    3rd Commit(macOS)
    Added working Health and Damage components. Hit Points <= 0 destroy object and attacker resumes walking.

--- Assets/Scripts/Attacker.cs
+++ Assets/Scripts/Attacker.cs
@@ -7,12 +7,21 @@ public class Attacker : MonoBehaviour
 {
     private float currentSpeed;
     private GameObject currentTarget;
+    private Animator anim;
 
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
 
     // Update is called once per frame
     void Update()
     {
         transform.Translate(Vector3.left * currentSpeed * Time.deltaTime);
+        if(!currentTarget)
+        {
+            anim.SetBool("isAttacking", false);
+        }
     }
 
     void OnTriggerEnter2D()
@@ -28,7 +37,15 @@ public class Attacker : MonoBehaviour
     // Caller from animator at time of actual blow
     public void StrikeCurrentTarget(float damage)
     {
-        Debug.Log(name + " caused damage: " + damage);
+        if (currentTarget)
+        {
+            Health health = currentTarget.GetComponent<Health>();
+            if(health)
+            {
+                health.DealDamage(damage);
+            }
+        }
+
     }
 
     public void Attack (GameObject obj)
---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour 
+{
+    public float hitPoints = 50f;
+
+    public void DealDamage (float damage)
+    {
+        hitPoints -= damage;
+        if (hitPoints <= 0)
+        {
+            //Optinally trigger death animation
+            DestroyObject();
+        }
+    }
+
+    public void DestroyObject()
+    {
+        Destroy(gameObject);
+    }
+}
