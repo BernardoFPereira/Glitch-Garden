8c0959296c5e2ccd68719a5d6a5be12ec8b86790
Bernardo F. Pereira <bernardopereiraf@gmail.com>
2018-07-16 18:55:24 -0300

    8th Commit(macOS)
    Defender instances spawn at precise points of the game space and are instanly parented to a "Defenders" object, keeping the hierarchy clean.

---
+++ Assets/Scripts/DefenderSpawner.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DefenderSpawner : MonoBehaviour 
+{
+    public Camera myCamera;
+
+    private GameObject parent;
+
+    //Find parent, create one if none available.
+    void Start()
+    {
+        parent = GameObject.Find("Defenders");
+
+        if(!parent)
+        {
+            parent = new GameObject("Defenders");
+        }
+    }
+
+    // Instantiate selected Defender at roundedPos position.
+    void OnMouseDown()
+    {
+        Vector2 rawPos = CalculateWorldPointOfMouseClick();
+        Vector2 roundedPos = SnapToGrid(rawPos);
+
+        GameObject defender = Button.selectedDefender;
+        Quaternion zeroRot = Quaternion.identity;
+        GameObject newDef = Instantiate(Button.selectedDefender, roundedPos, zeroRot) as GameObject;
+
+        newDef.transform.parent = parent.transform;
+    }
+
+    // Rounds click position in World Units
+    Vector2 SnapToGrid (Vector2 rawWorldPos) 
+    {
+        float snapX = Mathf.RoundToInt(rawWorldPos.x);
+        float snapY = Mathf.RoundToInt(rawWorldPos.y);
+        return new Vector2(snapX, snapY);
+    }
+
+    Vector2 CalculateWorldPointOfMouseClick()
+    {
+        float mouseX = Input.mousePosition.x;
+        float mouseY = Input.mousePosition.y;
+        float distanceFromCamera = 10f;
+
+        Vector3 weirdTriplet = new Vector3(mouseX, mouseY, distanceFromCamera);
+        Vector2 worldPos = myCamera.ScreenToWorldPoint(weirdTriplet);
+
+        return worldPos;
+    }
+}
